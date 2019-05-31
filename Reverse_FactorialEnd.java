3ecce3ba79d8458cb452b108021d928372e7a05c
Arturo <43208414+Escannor@users.noreply.github.com>
2019-05-31 14:53:01 -0500

    Create Reverse_FactorialEnd.java

---
+++ Reverse_FactorialEnd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(N%10);
+    }
+}
